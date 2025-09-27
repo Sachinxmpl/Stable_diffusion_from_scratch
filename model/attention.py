cc51912a6449fc3069740c26d69ca74b29dea1c1
Sachinxmpl <sachinxmpl6@gmail.com>
2025-09-27 22:49:39 +0545

    add self attention class

---
+++ model/attention.py
@@ -0,0 +1,62 @@
+import torch
+from torch import nn 
+from torch.nn import functional as F
+import math 
+
+class SelfAttention(nn.Module):
+    def __init__(self , num_heads , embed_dim):
+        """
+        Transformer multi-head attention 
+        """
+        super().__init__()
+
+        assert embed_dim % num_heads == 0 , "embed_dim must be divisible by num_heads"
+
+        self.embed_dim = embed_dim 
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+
+        self.q_linear = nn.Linear(embed_dim , embed_dim)
+        self.k_linear = nn.Linear(embed_dim , embed_dim)
+        self.v_linear = nn.Linear(embed_dim , embed_dim)
+
+        self.out_proj = nn.Linear(embed_dim , embed_dim)
+
+        self.attn_dropout = nn.Dropout(0.1)
+        self.proj_dropout = nn.Dropout(0.1)
+
+    def forward(self , x, causal_mask = False):
+        # x: (batch , seq_len , embed_dim)
+        batch , seq_len , embed_dim = x.shape
+
+        # computer Q , K , V 
+        Q = self.q_linear(x)  # (batch , seq_len , embed_dim)
+        K = self.k_linear(x)
+        V = self.v_linear(x)
+
+        # split into head 
+        # (batch , seq_len , embed_dim) --> (batch , seq_len , num_head , head_dim).T --> (batch , num_heads , seq_len , head_dim)
+        Q = Q.view(batch , seq_len , self.num_heads , self.head_dim).transpose(1,2)
+        K = K.view(batch , seq_len , self.num_heads , self.head_dim).transpose(1,2)
+        V = V.view(batch , seq_len , self.num_heads , self.head_dim).transpose(1,2)
+
+        # scores of dim (batch ,num_heads , seq_len , seq_len)
+        scores = torch.matmul(Q , K.transpose(-2,-1)) / math.sqrt(self.head_dim)
+
+        # add masking 
+        if causal_mask:
+            mask = torch.ones_like(scores, dtype=torch.bool).triu(1)   # upper triangle True
+            scores = scores.masked_fill(mask, float("-inf"))
+
+
+        attn = F.softmax(scores , dim = -1)
+        attn = self.attn_dropout(attn)
+
+        context = torch.matmul(attn , V)
+
+        # merge heads -> (batch , seq_len , embed_dim)
+        context = context.transpose(1,2).contiguous().view(batch , seq_len ,  self.embed_dim)
+        out = self.out_proj(context)
+        out = self.proj_dropout(out)
+        
+        return out
